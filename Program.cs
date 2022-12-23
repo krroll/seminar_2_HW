cf0cd556b231d38a3cae812d0290024ba349666b
Anton <krroll@bk.ru>
2022-12-24 00:31:01 +0800

    13 finish

--- Program.cs
+++ Program.cs
@@ -5,21 +5,38 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine (number);
+// int second = (number /10) % 10;
+// Console.WriteLine (second);
+
 //--------------------------------------------------------
 
-int number = new Random().Next(100,1000);
-Console.WriteLine (number);
-int second = (number /10) % 10;
-Console.WriteLine (second);
 // Задача 13: Напишите программу, которая выводит 
 // третью цифру заданного числа или сообщает, 
 // что третьей цифры нет.
 
 // 645 -> 5
-
 // 78 -> третьей цифры нет
-
 // 32679 -> 6
+
+int number = new Random().Next(10,1000);
+Console.WriteLine (number);
+
+int div10 = number / 10;
+if (div10 < 9)
+{
+    Console.WriteLine ("третьей цифры нет");
+}
+else
+{
+    int second = number %10;
+    Console.WriteLine (second);
+}
+
+
+
 //--------------------------------------------------------------
 // Задача 15: Напишите программу, которая принимает 
 // на вход цифру, обозначающую день недели, 
